0a49d5f8782ca8ac5be53873125bad6e5f545669
y1a2s3h4n5a6 <76025890+y1a2s3h4n5a6@users.noreply.github.com>
2021-05-28 11:28:05 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,23 @@
+class particle {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color=color(random(0,255), random(0,255), random(0,255))
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        noStroke();
+        fill("pink");
+       pop();
+    }
+
+}
